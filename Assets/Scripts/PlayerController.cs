50ef90d4757dc9a1c0045f988fdd7e866e688a6d
Anastasia Zhukova <anastasiya.zhukova@eltex.loc>
2017-03-07 15:49:15 +0700

    add script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horisontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+	}
+}
