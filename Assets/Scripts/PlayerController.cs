7831750c559ba8afd9ff9155912d41c31c3f6f1c
Anastasia Zhukova <anastasiya.zhukova@eltex.loc>
2017-03-09 16:46:06 +0700

    add rigidbody

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis ("Horisontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+	private Rigidbody rb;
 
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horisontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
+
+		rb.AddForce(movement);
 	}
 }
