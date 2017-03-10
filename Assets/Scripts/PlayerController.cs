6454db66dda84faf05ecc22fee393b9aed893ef4
Anastasia Zhukova <anastasiya.zhukova@eltex.loc>
2017-03-10 10:19:06 +0700

    ball is moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed;
 	private Rigidbody rb;
 
 	void Start() {
@@ -10,11 +11,11 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		float moveHorizontal = Input.GetAxis("Horisontal");
+		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 
 		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
 
-		rb.AddForce(movement);
+		rb.AddForce(movement * speed);
 	}
 }
